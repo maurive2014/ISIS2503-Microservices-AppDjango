e69eb48e2112a17065770c1c2453354042744658
o.urregov <o.urregov@uniandes.edu.co>
2024-05-19 19:35:27 -0500

    h

--- measurements/measurements/views.py
+++ measurements/measurements/views.py
@@ -50,7 +50,7 @@ def MeasurementsCreate(request):
         data_json = json.loads(data)
         measurement_list = []
         for measurement in data_json:
-                    if check_variable(measurement) == True and check_place(data_json) == True:
+                    if check_variable(measurement) == True and check_place(measurement) == True:
                         db_measurement = Measurement()
                         db_measurement.variable = measurement['variable']
                         db_measurement.value = measurement['value']
